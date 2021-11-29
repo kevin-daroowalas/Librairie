c0fb8cbec730f79acd1707071346e4d3cc6f49ff
kevin-daroowalas <79782083+kevin-daroowalas@users.noreply.github.com>
2021-11-29 13:02:15 -0500

    Added session , completed function ,removed 1
    I addded a session_start() function
    I added the variable for the return of the PDO object, the getter, setter and constructor
    I removed the update function cuz on the order is placed we cant update it only delete it

--- CommandeManager.php
+++ CommandeManager.php
@@ -1,58 +1,83 @@
 <?php
 
-declare(strict_types=1);
-
+//declare(strict_types=1);
+session_start();
 
 class CommandeManager
 {
+ //retour de l'objet de connection pdo
+    private $_db;
 
-
-    /**
-     * Default constructor
-     */
-    public function __construct()
+    //definir un constructeur
+    public function __construct($db)
     {
-        // ...
+        $this->setDb($db);
     }
 
-    /**
-     * 
-     */
+    //setter
+    public function setDb($db1)
+    {
+        $this->_db=$db1;
+    }
+
+    //getter
+    public function db()
+    {
+        return $this->_db;
+    }
+// list commade will show all the orders of everyone
     public function ListCommande()
     {
-        // TODO implement here
+        $req = $this->_db->query('SELECT * FROM commande');
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $u [] = new Commande($data);
+        }
+        return $u;
     }
 
     /**
      * 
      */
-    public function MyCommande()
+    public function MyCommande( Membres $m)
     {
-        // TODO implement here
+        $id= $m->Refer_Membre();
+         $req = $this->_db->query("SELECT * FROM Commande WHERE Refer_Membre ='".$id."'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $orders [] = new Commandes($data);
+        }
+        return $orders;
     }
 
-    /**
-     * 
-     */
-    public function PlacerCommande()
+   // function to place an order
+    public function PlacerCommande(Commandes $c)
     {
-        // TODO implement here
+        $query = $this->_db->prepare("INSERT into commande(RefCommande,Refer_Membre,Panier,Livraison,Prix) VALUES (:RefCommande,:Refer_Membre,:Panier,:Livraison,:Prix)");
+        
+        $query->bindValue(":RefCommande",$c->RefCommande());
+        $query->bindValue(":Refer_Membre",$c->Refer_Membre());
+        $query->bindValue(":Panier",$c->Panier());
+        $query->bindValue(":Livraison",$c->Livraison());
+        $query->bindValue(":Prix",$c->Prix());
+        $query->execute();
     }
 
-    /**
-     * 
-     */
+ 
+     // on peut pas update une commande
     public function UpdateCommande()
     {
         // TODO implement here
     }
-
-    /**
-     * 
-     */
-    public function AnnulerCommande()
+    // To Cancel order
+    public function AnnulerCommande(Commande $c)
     {
-        // TODO implement here
+       $id = $c->RefCommande();
+        $this->_db->exec("DELETE FROM commande WHERE RefCommande ='".$id."'");
     }
 
 }
