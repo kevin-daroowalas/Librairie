f5f9d4eae994ec28fa260ec2a46b705a95f450a2
kevin-daroowalas <79782083+kevin-daroowalas@users.noreply.github.com>
2021-11-29 14:59:52 -0500

    Added session,PDO,completed all functions not 1
    I added the session start function
    I implemented all the functions but not on cuz I'm not sure how to implement it 
    I added the PDO return object, getter, setter and constructor

--- OuvragesManager.php
+++ OuvragesManager.php
@@ -1,130 +1,260 @@
 <?php
 
-declare(strict_types=1);
-
+//declare(strict_types=1);
+session_start();
 
 class OuvragesManager
 {
 
+ //retour de l'objet de connection pdo
+    private $_db;
 
-    /**
-     * Default constructor
-     */
-    public function __construct()
+    //definir un constructeur
+    public function __construct($db)
     {
-        // ...
+        $this->setDb($db);
     }
 
-    /**
-     * 
-     */
+    //setter
+    public function setDb($db1)
+    {
+        $this->_db=$db1;
+    }
+
+    //getter
+    public function db()
+    {
+        return $this->_db;
+    }
+   
+    //List all the books
     public function ListAll()
     {
-        // TODO implement here
+         $req = $this->_db->query('SELECT * FROM Ouvrages');
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
     }
 
-    /**
-     * 
-     */
-    public function ListByName()
+    //List all Books by Title
+    public function ListByName( Ouvrages $o)
     {
-        // TODO implement here
+         $title = $o->Titre();
+         $req = $this->_db->query('SELECT * FROM Ouvrages WHERE Titre = '".$title."'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
     }
 
     /**
      * 
      */
-    public function ListByAuteur()
+    public function ListByAuteur( Ouvrages $o)
     {
-        // TODO implement here
+         $aut = $o->Auteur();
+         $req = $this->_db->query('SELECT * FROM Ouvrages WHERE Auteur = '".$aut."'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
     }
 
     /**
      * 
      */
-    public function ListByType()
+    public function ListByType( Ouvrages $o)
     {
-        // TODO implement here
+        $type = $o->Type();
+         $req = $this->_db->query('SELECT * FROM Ouvrages WHERE Type = '".$Type."'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
     }
 
     /**
      * 
      */
-    public function ListByGenre()
+    public function ListByGenre( Ouvrages $o)
     {
-        // TODO implement here
+         $genre = $o->Genre();
+         $req = $this->_db->query('SELECT * FROM Ouvrages WHERE Genre = '".$genre."'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
     }
 
     /**
      * 
      */
-    public function ListByMotCles()
+    public function ListByMotCles( Ouvrages $o)
     {
-        // TODO implement here
+         $mc = $o->MotCles();
+         $req = $this->_db->query('SELECT * FROM Ouvrages WHERE MotCles = '".$mc."'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
     }
 
-    /**
-     * 
-     */
-    public function ListByNbPages()
+    ///  IM TRYING SOMETHING ///
+   ///////////////////////////////
+    public function ListByNbPages( Ouvrages $o)
     {
-        // TODO implement here
-    }
+         $nbp=$_POST["val_pages"];
+         $req = $this->_db->query("SELECT * FROM Ouvrages WHERE NbPages BETWEEN 0 AND '".$nbp."'");
 
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
+    }
+/////////////////////////////////////////////
     /**
      * 
      */
-    public function ListByPrix()
+     ///  IM TRYING SOMETHING ///
+   ///////////////////////////////
+    public function ListByPrix( Ouvrages $o)
     {
-        // TODO implement here
+         $p=$_POST["val_pages"];
+         $req = $this->_db->query("SELECT * FROM Ouvrages WHERE Prix BETWEEN 0 AND '".$p."'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
     }
 
     /**
      * 
      */
-    public function ListByNameAndAuteur()
+    /////////////////////////////////////////////
+    public function ListByNameAndAuteur( Ouvrages $o)
     {
-        // TODO implement here
+        $title = $o->Titre();
+         $aut = $o->Auteur();
+         $req = $this->_db->query('SELECT * FROM Ouvrages WHERE Titre = '".$title."' AND Auteur ='".$aut."'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
     }
 
     /**
      * 
      */
-    public function ListByNameAuteurType()
+    public function ListByNameAuteurType( Ouvrages $o)
     {
-        // TODO implement here
+        $title = $o->Titre();
+         $aut = $o->Auteur();
+        $type = $o->Type();
+         $req = $this->_db->query('SELECT * FROM Ouvrages WHERE Titre = '".$title."' AND Auteur ='".$aut."' AND Type = '"$type"'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
     }
 
     /**
      * 
      */
-    public function ListByNameAuteurTypeGenre()
+    public function ListByNameAuteurTypeGenre( Ouvrages $o)
     {
-        // TODO implement here
+        $title = $o->Titre();
+         $aut = $o->Auteur();
+        $type = $o->Type();
+        $genre = $o->Genre();
+         $req = $this->_db->query('SELECT * FROM Ouvrages WHERE Titre = '".$title."' AND Auteur ='".$aut."' AND Type = '".$type."' AND Genre ='".$genre."'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
     }
 
     /**
      * 
      */
-    public function ListByNameAuteurTypeGenreMotCles()
+    public function ListByNameAuteurTypeGenreMotCles( Ouvrages $o)
     {
-        // TODO implement here
+       $title = $o->Titre();
+         $aut = $o->Auteur();
+        $type = $o->Type();
+        $genre = $o->Genre();
+         $mc = $o->MotCles();
+         $req = $this->_db->query('SELECT * FROM Ouvrages WHERE Titre = '".$title."' AND Auteur ='".$aut."' AND Type = '".$type."' AND Genre ='".$genre."' AND Motcles = '".$mc."'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
     }
 
     /**
      * 
      */
+    // I DONT KNOW HOW TO IMPLEMENT THAT
+    //////////////////////////
     public function ListByAll()
     {
-        // TODO implement here
-    }
+        $title = $o->Titre();
+        $aut = $o->Auteur();
+        $type = $o->Type();
+        $genre = $o->Genre();
+        $mc = $o->MotCles();
+        $an_p = $o->Annee_Pub();
+        $p = $o->Prix();
+        $nbp=$_POST["val_pages"];
+         $req = $this->_db->query('SELECT * FROM Ouvrages WHERE Titre = '".$title."' AND Auteur ='".$aut."' AND Type = '".$type."' AND Genre ='".$genre."' AND Motcles = '".$mc."'");
 
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
+        }
+    }
+//////////////////////////////////////////////////////////////
     /**
      * 
      */
     public function ListByNameAuteurTypeGenreMotClesPages()
     {
-        // TODO implement here
+        $title = $o->Titre();
+        $aut = $o->Auteur();
+        $type = $o->Type();
+        $genre = $o->Genre();
+        $mc = $o->MotCles();
+        $nbp=$_POST["val_pages"];
+         $req = $this->_db->query('SELECT * FROM Ouvrages WHERE NbPages BETWEEN 0 AND '".$nbp."'AND Titre = '".$title."' AND Auteur ='".$aut."' AND Type = '".$type."' AND Genre ='".$genre."' AND Motcles = '".$mc."'");
+
+        while($data=$req->fetch(PDO::FETCH_ASSOC))
+        {
+           
+            $users [] = new Ouvrages($data);
     }
 
 }
